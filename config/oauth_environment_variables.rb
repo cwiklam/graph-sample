479f611e24f95db013c45255b24a8420069c6851
cwiklam <cwikla.m0@gmail.com>
2022-11-19 09:56:13 +0100

    new app for rails 7

---
+++ config/oauth_environment_variables.rb
@@ -0,0 +1,3 @@
+ENV['AZURE_APP_ID'] = 'a70f6933-16cb-4d34-88d5-7669e4b61f57'
+ENV['AZURE_APP_SECRET'] = 'DQN8Q~cnJ2qAJLHlvzVejwV4nQO78JgFZ-g1WbwS'
+ENV['AZURE_SCOPES'] = 'openid profile email offline_access user.read mailboxsettings.read calendars.readwrite'
