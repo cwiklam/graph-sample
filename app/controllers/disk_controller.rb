62ad1f6cd7a077437fa97ba3417e670b66f7b115
cwiklam <cwikla.m0@gmail.com>
2022-11-26 12:43:05 +0100

    Sending working

--- app/controllers/disk_controller.rb
+++ app/controllers/disk_controller.rb
@@ -13,18 +13,37 @@ class DiskController < ApplicationController
 
   end
 
+  # TODO https://www.example-code.com/ruby/onedrive_upload_binary_data_from_memory.asp tutorial to fix
   def create
     http = Chilkat::CkHttp.new()
-    content = File.read(params[:picture].tempfile)
+    file_access = Chilkat::CkFileAccess.new()
+    file_access.OpenForRead(params[:picture].tempfile.path)
     file = Chilkat::CkBinData.new()
-    file.AppendEncoded(content, 'UTF-8')
+    # file_access.AppendBd(file)
+    file_access.FileReadBd(99999999, file)
     req = Chilkat::CkHttpRequest.new()
     req.put_HttpVerb("PUT")
     req.put_Path("/v1.0/drives/b!4RLrdQRYMESPvzn0DdDqqAqGVRfGWQ5DphR78v4tUrJUN3JKcWcZR5ZgBRL_7WsY/items/01VRBOIKPBEO3H4OPHT5FL5EGYOJUGVA57:/file_icon.png:/content")
     req.LoadBodyFromBd(file)
     req.put_ContentType("application/octet-stream")
+    req.AddHeader('Authorization', "Bearer #{session[:graph_token_hash][:token]}")
     resp = http.SynchronousRequest("graph.microsoft.com",443,true,req)
-    # TODO https://www.example-code.com/ruby/onedrive_upload_binary_data_from_memory.asp tutorial to fix
-    binding.pry
+    if (http.get_LastMethodSuccess() != true)
+      print http.lastErrorText() + "\n";
+      exit
+    end
+    json = Chilkat::CkJsonObject.new()
+    json.put_EmitCompact(false)
+    json.Load(resp.bodyStr())
+
+    if (resp.get_StatusCode() != 201)
+      print json.emit() + "\n";
+      print "Response status = " + resp.get_StatusCode().to_s() + "\n";
+
+      exit
+    end
+    print json.emit() + "\n";
+    print "@@ Success @@" + "\n";
+    file_access.FileClose()
   end
 end
